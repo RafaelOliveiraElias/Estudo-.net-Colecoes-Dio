abbb9cf6b8f2f78eddbdaa9631c72c2504c64eb2
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-06-21 18:14:29 -0300

    learning

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Colecoes.Helper;
+
+OperacoesArray op = new OperacoesArray();
+int[] array = new int[5] { 6, 3, 8, 1, 9 };
+System.Console.WriteLine("array original");
+op.ImprimirArray(array);
+
+System.Console.WriteLine("array ordenado");
+
+op.OrdenarBubbleSort(ref array);
+op.ImprimirArray(array);
+
+
+// int [] arrayInteiros = new int[3];
+
+// arrayInteiros[0] = 10;
+// arrayInteiros[1] = 20;
+// arrayInteiros[2] = int.Parse("30");
+
+
+// System.Console.WriteLine("array pelo for");
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//   System.Console.WriteLine(arrayInteiros[i]);
+// }
+
+
+// System.Console.WriteLine("array pelo forEach");
+// foreach (int valor in arrayInteiros)
+// {
+//   System.Console.WriteLine(valor);
+// }
+
+// int[,] matriz = new int[4, 2]
+// {
+//   { 8, 8 },
+//   { 10, 20 },
+//   { 50, 100 },
+//   { 90, 200 }
+// };
+
+// for (int i = 0; i < matriz.GetLength(0); i++)
+// {
+//   for (int j = 0; j < matriz.GetLength(1) ; j++)
+//   {
+//     System.Console.WriteLine(matriz[i, j]);
+//   }
+// }
